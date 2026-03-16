cc6b95e1b5f41870d58d7fe0746cd46ebb919a99
Christian Michael Kessler <148792985+chrismich-k@users.noreply.github.com>
2026-03-16 12:56:50 -0400

    fix: add missing unit tests for TestProductModel

--- tests/test_models.py
+++ tests/test_models.py
@@ -72,7 +72,12 @@ class TestProductModel(unittest.TestCase):
 
     def test_create_a_product(self):
         """It should Create a product and assert that it exists"""
-        product = Product(name="Fedora", description="A red hat", price=12.50, available=True, category=Category.CLOTHS)
+        product = Product(
+            name="Fedora",
+            description="A red hat",
+            price=12.50, available=True,
+            category=Category.CLOTHS
+        )
         self.assertEqual(str(product), "<Product Fedora id=[None]>")
         self.assertTrue(product is not None)
         self.assertEqual(product.id, None)
@@ -101,6 +106,151 @@ class TestProductModel(unittest.TestCase):
         self.assertEqual(new_product.available, product.available)
         self.assertEqual(new_product.category, product.category)
 
-    #
-    # ADD YOUR TEST CASES HERE
-    #
+    def test_read_product(self):
+        """It should read a newly created object back from the DB and check whether it matches"""
+        new_product = ProductFactory()
+        app.logger.debug("created a new product object: {new_product}")
+
+        # persist in DB
+        new_product.id = None
+        new_product.create()
+        self.assertIsNotNone(new_product.id)
+
+        # query product from DB
+        read_product = Product.find(new_product.id)
+
+        # check if attributes match the created object
+        self.assertEqual(read_product.id, new_product.id)
+        self.assertEqual(read_product.name, new_product.name)
+        self.assertEqual(read_product.description, new_product.description)
+        self.assertEqual(read_product.available, new_product.available)
+        self.assertEqual(read_product.price, new_product.price)
+        self.assertEqual(read_product.category, new_product.category)
+
+    def test_update_product(self):
+        """It should update an object and assert the changes are persisted"""
+        new_product = ProductFactory()
+        app.logger.debug("created a new product object: {new_product}")
+
+        # persist in DB
+        new_product.id = None
+        new_product.create()
+        app.logger.debug("persisted the new product in DB: {new_product}")
+
+        # update attributes and update in DB
+        original_description = new_product.description
+        new_description = "This is a test description."
+        new_product.description = new_description
+        new_product.update()
+
+        # assert the description is set and there is an id
+        self.assertIsNotNone(new_product.id)
+        self.assertEqual(new_description, new_product.description)
+
+        # fetch all products from DB and assert it's one entry
+        all_products = Product.all()
+        self.assertEqual(len(all_products), 1)
+
+        # assert the description has changed and id is the same
+        self.assertNotEqual(original_description, all_products[0].description)
+        self.assertEqual(new_product.id, all_products[0].id)
+
+    def test_delete_product(self):
+        """It should delete a newly created test object and validate it's deleted"""
+        new_product = ProductFactory()
+        app.logger.debug("created a new product object: {new_product}")
+
+        # persist in DB
+        new_product.id = None
+        new_product.create()
+        app.logger.debug("persisted the new product in DB: {new_product}")
+
+        # fetch all products from DB and assert it's one entry
+        all_products = Product.all()
+        self.assertEqual(len(all_products), 1)
+
+        # delete the test product and assert it's deleted (no objects in DB)
+        all_products[0].delete()
+        remaining_products = Product.all()
+        self.assertEqual(len(remaining_products), 0)
+
+    def test_all_five_products(self):
+        """It should create 5 products in an empty DB and assert there are 5 products in DB"""
+        # fetch all products from DB and assert it's empty
+        products = Product.all()
+        self.assertEqual(len(products), 0)
+
+        # create five test products
+        for _ in range(5):
+            new_product = ProductFactory()
+            new_product.create()
+
+        # get all products and assert it's five of them
+        products = Product.all()
+        self.assertEqual(len(products), 5)
+
+    def test_find_products_by_name(self):
+        """It should test whether find_by_name() correctly filters products"""
+        # create five test products
+        for _ in range(5):
+            new_product = ProductFactory()
+            new_product.create()
+
+        # get first product from list of all products
+        all_products = Product.all()
+        first_name = all_products[0].name
+
+        # count all products with the same name as first product
+        count = sum(1 for product in all_products if product.name == first_name)
+
+        # assert Product.find_by_name() finds the same number of matches
+        filtered_products = Product.find_by_name(first_name).all()
+        self.assertEqual(len(filtered_products), count)
+
+        # assert all found products have the name we're loooking for
+        count = sum(1 for product in filtered_products if product.name == first_name)
+        self.assertEqual(len(filtered_products), count)
+
+    def test_find_products_by_availability(self):
+        """It should test whether find_by_availability() correctly filters products"""
+        # create ten test products
+        for _ in range(10):
+            new_product = ProductFactory()
+            new_product.create()
+
+        # get first product from list of all products
+        all_products = Product.all()
+        first_available = all_products[0].available
+
+        # count all products with the same availability as the first product
+        count = sum(1 for product in all_products if product.available == first_available)
+
+        # assert Product.find_by_availability() finds the same number of matches
+        filtered_products = Product.find_by_availability(first_available).all()
+        self.assertEqual(len(filtered_products), count)
+
+        # assert all found products have the availability we're loooking for
+        count = sum(1 for product in filtered_products if product.availabley == first_available)
+        self.assertEqual(len(filtered_products), count)
+
+    def test_find_products_by_category(self):
+        """It should test whether find_by_category() correctly filters products"""
+        # create ten test products
+        for _ in range(10):
+            new_product = ProductFactory()
+            new_product.create()
+
+        # get first product's category from list of all products
+        all_products = Product.all()
+        first_category = all_products[0].category
+
+        # count all products with that category
+        count = sum(1 for product in all_products if product.category == first_category)
+
+        # assert Product.find_by_availability() finds the same number of matches
+        filtered_products = Product.find_by_category(first_category).all()
+        self.assertEqual(len(filtered_products), count)
+
+        # assert all found products have the category we're loooking for
+        count = sum(1 for product in filtered_products if product.category == first_category)
+        self.assertEqual(len(filtered_products), count)
